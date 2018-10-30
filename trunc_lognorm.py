8eef9019958db1249233f1cc711e1aa9252cfefc
Duncan Campbell <duncan.campbell@yale.edu>
2018-10-30 10:38:37 -0400

    add ppf

--- trunc_lognorm.py
+++ trunc_lognorm.py
@@ -16,36 +16,55 @@ class TruncLogNorm(rv_continuous):
     truncted log-normal distribution
     """
 
-    def _argcheck(self, a, b, mu, sigma):
+    def _argcheck(self, a, b, s):
         r"""
         check arguments
         """
         self.a = a  # lower bound
         self.b = b  # upper bound
-        return (self.a >= 0.0) & (self.b > self.a)
+        return (self.a >= 0.0) & (self.b > self.a) & (s>0.0)
 
-    def n1(a, b, mu, sigma):
-        return 0.5 + 0.5*erf((np.log(a)-mu)/(np.sqrt(2)*sigma))
+    def _n1(self, a, b, s):
+        r"""
+        """
+        with np.errstate(divide='ignore', invalid='ignore'):
+            return np.where(a>0.0, 0.5 + 0.5*erf((np.log(a))/(np.sqrt(2)*s)), 0.0)
 
-    def n2(a, b, mu, sigma):
-        return 1.0 - 0.5 + 0.5*erf((np.log(b)-mu)/(np.sqrt(2)*sigma))
+    def _n2(self, a, b, s):
+        r"""
+        """
+        return 1.0 - 0.5 + 0.5*erf((np.log(b))/(np.sqrt(2)*s))
 
-    def norm(self, a, b):
+    def _norm(self, a, b, s):
+        r"""
+        """
+        return 1.0/(s * np.sqrt(2.0*np.pi)) * 1.0/(self._n1(a, b, s) + self._n2(a, b, s))
+
+    def _f_trunc(self, a, b, s):
+        r"""
+        """
+        return self._norm(a, b, s)/self._norm(0.0, np.inf, s)
+
+    def _pdf(self, x, a, b, s):
+        r"""
+        """
+        norm = self._norm(a, b, s)
+        with np.errstate(divide='ignore', invalid='ignore'):
+            return np.where((x>a) & (x<=b), (norm/x)*np.exp(-1.0*(np.log(x))**2/(2.0*s**2)), 0.0)
+
+    def _cdf(self, x, a, b, s):
         """
         """
-        return 1.0/(sigma*np.sqrt(2.0*np.pi))* 1.0/(n1(a, b, mu, sigma) + n2(a, b, mu, sigma))
-
-    def _pdf(self, x, a, b, mu, sigma):
-        """
-        """
-        norm = self.norm(a, b)
-        return np.where((x>a) & (x<=b), norm/x*np.exp(-1.0*(np.log(x)-mu)**2/(2.0*sigma**2)), 0.0)
-
-    def _cdf(self, x, a, b, mu, sigma):
-        cdf = 0.5 + 0.5*erf((np.log(a)-mu)/(np.sqrt(2)*sigma)) - n1(a, b, mu, sigma)
+        cdf = 0.5 + 0.5 * erf((np.log(x))/(np.sqrt(2.0) * s)) - self._n1(a, b, s)
+        cdf = cdf/self._n2(a, b, s)
         cdf = np.where(x>a, cdf, 0.0)
         cdf = np.where(x<=b, cdf, 1.0)
         return cdf
-    
-    def _ppf(self, q, a, b, mu, sigma):
-        
+
+    def _ppf(self, q, a, b, s):
+        r"""
+        """
+        q = q*self._n2(a, b, s)
+        ppf = np.exp(erfinv(2.0*q-1.0+2.0*self._n1(a, b, s))*np.sqrt(2.0*s))
+        return ppf
+
